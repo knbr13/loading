48c863105dfbd7d87061bd95f6063b83a83be961
knbr13 <abdullahalaadine63@gmail.com>
2025-01-23 00:06:24 +0200

    validate config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 
@@ -17,13 +18,36 @@ func main() {
 		log.Fatalf("error loading config: %v", err)
 	}
 
+	err = validateCfg(cfg)
+	if err != nil {
+		log.Fatalf("error validating configurations: %v", err)
+	}
+
+	var i *int
+	if cfg.RequestCount != nil {
+		j := int(*cfg.RequestCount)
+		i = &j
+	}
+
 	options := loader.RequestOptions{
 		Method:       cfg.Method,
 		URL:          cfg.TargetURL,
 		Headers:      cfg.Headers,
 		Concurrency:  cfg.Concurrency,
-		RequestCount: cfg.RequestCount,
+		Duration:     cfg.Duration,
+		RequestCount: i,
 	}
 
 	loader.LoadTest(options)
+}
+
+func validateCfg(c config.Config) error {
+	if (c.Duration == nil && c.RequestCount == nil) ||
+		(c.Duration != nil && c.RequestCount != nil) {
+		return errors.New("provide a value for one of 'request_count' or 'duration' (one of them only, not both)")
+	}
+	if c.Concurrency <= 0 {
+		return errors.New("'concurrency' value should be greater than zero")
+	}
+	return nil
 }
